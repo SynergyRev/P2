59ea613182c0031b1102f624dfc855ad25100304
Your Name <you@example.com>
2022-06-15 10:42:37 -0400

    Frontend

--- src/main/java/com/revature/Launcher.java
+++ src/main/java/com/revature/Launcher.java
@@ -1,28 +1,41 @@
 package com.revature;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.revature.utilities.HibernateUtil;
+
 import models.Users;
 import repository.UserDao;
 
 public class Launcher {
 public static void main(String[] args) {
 	
+	try(Session ses = HibernateUtil.getSession()){
+		System.out.println("Connection Successful");
+}
+	catch (HibernateException e) {
+	System.out.println("Connection Failed");
+		e.printStackTrace();
+	}
+	
 
 UserDao ud = new UserDao();
 
 
 
-Users u1 = new Users("Momo", "Traore", "MomoT1", "password","momotraore@yahoo.com", "New York");
-Users u2 = new Users("Aly", "Traore", "AlyT1", "password","alytraore@yahoo.com", "New York");
-//Users u3 = new Users();
-//Users u4 = new Users();
-
-
-
-ud.insertUser(u1);
-ud.insertUser(u2);
-//ud.insertUser(u3);
-//ud.insertUser(u4);
+//Users u1 = new Users("Momo", "Traore", "MomoT1", "password","momotraore@yahoo.com", "New York");
+//Users u2 = new Users("Aly", "Traore", "AlyT1", "password","alytraore@yahoo.com", "New York");
+////Users u3 = new Users();
+////Users u4 = new Users();
 //
-//	
+//
+//
+//ud.insertUser(u1);
+//ud.insertUser(u2);
+////ud.insertUser(u3);
+////ud.insertUser(u4);
+////
+////	
 }
 }
--- src/main/java/com/revature/utilities/HibernateUtil.java
+++ src/main/java/com/revature/utilities/HibernateUtil.java
@@ -1,16 +1,18 @@
 package com.revature.utilities;
 
+
 import org.hibernate.Session;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+
+
 public class HibernateUtil {
 	
-private static SessionFactory sf = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+	private static SessionFactory sf = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 	
 	private static Session ses;
-	
-	
 	
 	public static Session getSession() {
 		if(ses == null) { //if there isn't a session
